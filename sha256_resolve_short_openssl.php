f2d853cb82a47e4086bff9ec938581333ca12aad
uzulla <uzulla@himitsukichi.com>
2021-02-20 18:39:26 +0900

    add use openssl_digest version.

--- sha256_resolve_short_openssl.php
+++ sha256_resolve_short_openssl.php
@@ -1,5 +1,5 @@
 <?php
-// TTILE: SHA256 hash seed resolver without gmp (as a joke).
+// TTILE: SHA256 hash seed resolver use openssl_digest (as a joke).
 // AUTHOR: uzulla(Junichi ISHIDA) <zishida@gmail.com>
 // LICENSE: MIT (https://opensource.org/licenses/MIT)
 //
@@ -10,12 +10,15 @@
 // The code will be overflow when $i value overflow PHP_INT_MAX. this is sample.
 // php > echo dechex(PHP_INT_MAX);
 // 7fffffffffffffff
+//
+// ## note
+// seems as same as speed hash() and openssl_digest.
 
 if (!isset($argv[1])) die("please set target hash");
 $hash = (string)$argv[1];
 
 $i = 0;
-while (hash('sha256', dechex($i)) !== $hash) {
+while (openssl_digest(dechex($i), 'sha256') !== $hash) {
   $i++;
 }
 echo "resolved!(?) result is `". dechex($i) ."`" . PHP_EOL;
